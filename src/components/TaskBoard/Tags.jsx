4c0e4495c33933efbcc9ca76913f7bfc72f20d1c
shaakilahammed <shaakilahammed@gmail.com>
2024-01-16 20:25:14 +0600

    list render

---
+++ src/components/TaskBoard/Tags.jsx
@@ -0,0 +1,37 @@
+const colors = [
+  '00D991A1',
+  '1C92FFB0',
+  'FE1A1AB5',
+  'BD560BB2',
+  '00B2D9CC',
+  '8407E6A8',
+  '07AC67D6',
+  '2F43F8BF',
+  'AE6D0BDB',
+  '10FBEDB2',
+];
+const Tag = ({ name }) => {
+  return (
+    <li>
+      <span
+        className={`inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#${
+          colors[Math.floor(Math.random() * colors.length)]
+        }] px-2.5 text-sm capitalize text-[#F4F5F6]`}
+      >
+        {name}
+      </span>
+    </li>
+  );
+};
+
+const Tags = ({ tags }) => {
+  return (
+    <ul className="flex justify-center gap-1.5 flex-wrap">
+      {tags.map((tag, index) => (
+        <Tag key={index} name={tag} />
+      ))}
+    </ul>
+  );
+};
+
+export default Tags;
--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import SearchBox from './SearchBox';
 import TaskActions from './TaskActions';
 import TaskList from './TaskList';
 
+const data = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Learn React',
+    description:
+      'React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.',
+    tags: ['web', 'javascript', 'react'],
+    priority: 'High',
+    isFavourite: false,
+  },
+];
+
 const TaskBoard = () => {
+  const [tasks, setTasks] = useState(data);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -10,7 +24,7 @@ const TaskBoard = () => {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
 
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
